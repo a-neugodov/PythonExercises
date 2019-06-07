7db612cc61887e5ab7e235a262be8c896e345d44
a-neugodov <40794785+a-neugodov@users.noreply.github.com>
2019-06-07 17:00:53 +0300

    Update DNAtoRNA.py

--- DNAtoRNA.py
+++ DNAtoRNA.py
@@ -1,7 +1,9 @@
-def f(t):
-    if len(t) <= 1000:
-        u = t.replace('T','U')
+t = input(f'Type DNA sequence: ')
+t_upper = t.upper()
+def f(t_upper):
+    if len(t_upper) <= 1000:
+        u = t_upper.replace('T','U')
         print(f'RNA sequence: {u}')
     else:
         print ('DNA sequence shoud not contain more then 1000 nk!')
-f(input(f'Type DNA sequence: '))
+f(t_upper)
